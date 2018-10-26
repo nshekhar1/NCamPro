a10f8b6109019fdb30cda9874a1c5676e400d497
kavanmehta <kavan1992.mehta23@gmail.com>
2018-10-26 11:46:55 +0530

    initial code setup

---
+++ NCamPro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NCamPro.MvcApplication" Language="C#" %>
---
+++ NCamPro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NCamPro.Startup))]
+namespace NCamPro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
